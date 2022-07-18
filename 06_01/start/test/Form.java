0dc5cf1e1d99cbdd577e6011c7cb2aeb6e8e721a
pannaraiSIH <pannarai.sih@student.mahidol.edu>
2022-07-18 15:45:58 +0700

    Initial commt

---
+++ 06_01/start/test/Form.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Form {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/panna/Downloads/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        driver.findElement(By.id("first-name")).sendKeys("Stang");
+        driver.findElement(By.id("last-name")).sendKeys("Sihongthong");
+        driver.findElement(By.id("job-title")).sendKeys("QA");
+        driver.findElement(By.id("radio-button-2")).click();
+        driver.findElement(By.id("checkbox-2")).click();
+        driver.findElement(By.cssSelector("option[value='1'")).click();
+        driver.findElement(By.id("datepicker")).sendKeys("17/07/22");
+        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
+        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+
+        driver.quit();
+    }
+}
